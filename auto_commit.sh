70229c963ffc6ca9527a5bc7848ef9c1d71fa07a
osama mohamoud <wwwhdosama@gmail.com>
2025-06-17 21:06:45 +0300

    Auto commit: 2025-06-17 21:06:45

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,11 +2,11 @@
 
 cd "E:/projects/c++ course/ELzero course c++ level one" || exit
 
-git pull origin master
+git pull origin main
 
 git add -u
 git add .
 
 git commit -m "Auto commit: $(date '+%Y-%m-%d %H:%M:%S')" --quiet
 
-git push origin master --quiet
+git push origin main --quiet
