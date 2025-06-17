0eba30cc694d1126d37c078c409366a8bc7f4232
osama mohamoud <wwwhdosama@gmail.com>
2025-06-17 21:05:50 +0300

    Auto commit: 2025-06-17 21:05:49

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Move to the repo directory
 cd "E:/projects/c++ course/ELzero course c++ level one" || exit
 
-# Update the repo
-git pull origin main
+git pull origin master
 
-# Add all changes, but respect .gitignore automatically
-git add -u   # add updated and deleted files
-git add .    # add new files except ignored by .gitignore
+git add -u
+git add .
 
-# Commit with date and time automatically
 git commit -m "Auto commit: $(date '+%Y-%m-%d %H:%M:%S')" --quiet
 
-# Push changes to remote
-git push origin main --quiet
+git push origin master --quiet
