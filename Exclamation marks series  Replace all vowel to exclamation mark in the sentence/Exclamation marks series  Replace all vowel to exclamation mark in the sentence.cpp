3ca9b2b251463ea8bf587fd3bbe371e86d3ce049
osama mohamoud <wwwhdosama@gmail.com>
2025-06-18 18:46:45 +0300

    Auto commit: 2025-06-18 18:46:45

--- Exclamation marks series  Replace all vowel to exclamation mark in the sentence/Exclamation marks series  Replace all vowel to exclamation mark in the sentence.cpp
+++ Exclamation marks series  Replace all vowel to exclamation mark in the sentence/Exclamation marks series  Replace all vowel to exclamation mark in the sentence.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 string input() {
 	string w;
 	cout << "enter word: ";
-	cin >> w;
+	getline(cin, w);
+	
 	return w;
 }
 void vowel_remove(string w) {
 	string word;
 	for (int i = 0; i < w.length(); i++) {
+
 		if (w[i] == 'a' || w[i] == 'e' || w[i] == 'i' || w[i] == 'o' || w[i] == 'u') {
 		
+			 
+				word += '!';
+			continue;
+			
+		}
+		else if ( isupper(w[i]) &&(w[i] == 'A' || w[i] == 'E' || w[i] == 'I' || w[i] == 'O' || w[i] == 'U') )  {
 			word += '!';
-				continue;
+			continue;
 		}
 		else  word += w[i];
 	}
