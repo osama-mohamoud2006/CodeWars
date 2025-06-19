eb2c3991b3e036b3bedfe47b4dd9575dd6803c82
osama mohamoud <wwwhdosama@gmail.com>
2025-06-19 22:09:10 +0300

    Auto commit: 2025-06-19 22:09:09

--- Reversed Words/Reversed Words.cpp
+++ Reversed Words/Reversed Words.cpp
@@ -1,20 +1,36 @@
-// Reversed Words.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+  int main() {
+
+    string word;
+    cout << "enter sentence: ";
+    getline(cin, word);
+
+    string first ;
+	string arr[100];
+    // first word 
+    for (int i = 0; i < word.length(); i++) {
+        if (word[i] != ' ') first += word[i]; // if word isn't space 
+
+        else {
+			arr[i] = first; // store the word in array
+			
+           first = "";
+           
+        }
+    }
+
+    if (first!= "") {  // for the last word 
+		arr[word.length()] = first; // store the last word
+    
+    }
+
+    for(int i = word.length(); i >= 0; i--) { // print the words in reverse order
+        if (arr[i] != "") {
+            cout  << arr[i] << " ";
+        }
+	}
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
